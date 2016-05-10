23e66f31e859797c5f71f46538c5ceba7f20a367
lensvelt <lensvelt.brett@gmail.com>
2016-05-09 18:44:05 -0700

    working on request-handler

--- client/env/config.js
+++ client/env/config.js
@@ -13,7 +13,7 @@ if (!/(&|\?)username=/.test(window.location.search)) {
 }
 
 // Put your parse application keys here!
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', '6UJYuifdHSHnOvG2DiYXU6cwluUvgDiVOpr8Weqi');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'gYu7Z35zwiNz4BaNgwyaq9u9A36eVHd38MiDvCH5');
-});
+// $.ajaxPrefilter(function (settings, _, jqXHR) {
+//   jqXHR.setRequestHeader('X-Parse-Application-Id', '6UJYuifdHSHnOvG2DiYXU6cwluUvgDiVOpr8Weqi');
+//   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'gYu7Z35zwiNz4BaNgwyaq9u9A36eVHd38MiDvCH5');
+// });
